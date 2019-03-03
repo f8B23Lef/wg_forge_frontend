050516846771cb243bd6f6eb02f48d0e29f327bb
f8B23Lef <mal7rish@yandex.ru>
2019-03-03 18:55:07 +0300

    feat: implement third task + make a few corrections

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import orders from '../data/orders.json';
 import users from '../data/users.json';
+import companies from '../data/companies.json';
 
 import './app.css';
 
 const formDate = (dateUnix) => {
-  let date = new Date(Number(dateUnix)).toLocaleString('en-US').split('/');
-  [date[1], date[0]] = [date[0], date[1]]
-
-  return date.join('/');
+  const dateTime = new Date(Number(dateUnix));
+  const date = dateTime.toLocaleDateString('en-GB');
+  const time = dateTime.toLocaleTimeString('en-GB');
+  
+  return date.concat(' ', time);
 };
 
 const formCardNumber = (cardNumber) => {
@@ -16,11 +18,44 @@ const formCardNumber = (cardNumber) => {
     });
 };
 
-const formUserInfo = (userId) => {
-  const user = users.find(el => el.id === userId);
-  const fullName = user.first_name.concat(' ', user.last_name);
+const getUser = (userId) => {
+  return users.find(el => el.id === userId);
+};
 
-  return user.gender === 'Male' ? 'Mr.'.concat(' ', fullName): 'Ms.'.concat(' ', fullName);
+const formUserFullName = (firstName, lastName, gender) => {
+  const fullName = firstName.concat(' ', lastName);
+
+  return gender === 'Male' ? 'Mr.'.concat(' ', fullName): 'Ms.'.concat(' ', fullName);
+};
+
+const formUserBirthday = (birthday) => {
+  return new Date(Number(birthday)).toLocaleDateString('en-GB');
+};
+
+const getCompany = (companyId) => {
+  return companies.find(el => el.id === companyId);
+};
+
+const formUserDetailsBlock = (user, company) => {
+  const birthday = user.birthday
+    ? `<p>Birthday: ${formUserBirthday(user.birthday)}</p>`
+    : '';
+
+  const avatar = user.avatar 
+    ? `<p><img src="${user.avatar}" alt="user avatar" width="100px"></p>`
+    : '';
+
+  let companyInfo = '';
+  if (company) {
+    if (company.url) {
+      companyInfo += `<p>Company: <a href="${company.url}" target=_blank>${company.title}</a></p>`;
+    } else {
+      companyInfo += `<p>Company: ${company.title}</p>`;
+    }
+    companyInfo += `<p>Industry: ${company.industry} / ${company.sector}</p>`;
+  }
+
+  return birthday.concat(avatar, companyInfo);
 };
 
 const renderTable = () => {
@@ -38,11 +73,20 @@ const renderTable = () => {
       </tr>
     </thead>
   <tbody>`;
+
   for (let i = 0; i < orders.length; i += 1) {
+    const user = getUser(orders[i].user_id);
+    const company = getCompany(user.company_id);
+
     table += `
     <tr id="order_${orders[i].id}">
       <td>${orders[i].transaction_id}</td>
-      <td class="user_data"><a href="#">${formUserInfo(orders[i].user_id)}</a></td>
+      <td class="user-data">
+        <a class="user-name" href="#">${formUserFullName(user.first_name, user.last_name, user.gender)}</a>
+        <div class="user-details hidding">
+          ${formUserDetailsBlock(user, company)}
+        </div>
+      </td>
       <td>${formDate(orders[i].created_at)}</td>
       <td>$${orders[i].total}</td>
       <td>${formCardNumber(orders[i].card_number)}</td>
@@ -50,6 +94,7 @@ const renderTable = () => {
       <td>${orders[i].order_country} (${orders[i].order_ip})</td>
     </tr>`;
   }
+
   table += `
     </tbody>
   </table>`;
@@ -58,5 +103,14 @@ const renderTable = () => {
 };
 
 export default (function () {
-    document.getElementById("app").innerHTML = `<section>${renderTable()}</section>`;
+  document.getElementById("app").innerHTML = renderTable();
+
+  document.querySelectorAll('a.user-name').forEach((link) => {
+    link.addEventListener('click', (e) => {
+      e.preventDefault();
+      link.nextElementSibling.classList.toggle('hidding');
+    });
+  });
 }());
+
+//q.match(/^\$([\d,.]+)/);
